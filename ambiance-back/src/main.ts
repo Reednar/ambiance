2bbb5a872791110bcaea23451c7cc0af2427a665
LoiÌc LEPIE [386ea5b5-1a6a-4978-93dc-42e69c2e1962] <loic.lepie@ca-ps.com>
2024-10-24 11:02:19 +0200

    update config for deploy

--- ambiance-back/src/main.ts
+++ ambiance-back/src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+const envFile = `.env.${process.env.NODE_ENV || 'preproduction'}`;
+dotenv.config({ path: envFile });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.setGlobalPrefix('api');
-
-  app.enableCors();
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
---
+++ ambiance-front/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+// ambiance-front/src/environments/environment.prod.ts
+export const environment = {
+    production: true,
+    apiBaseUrl: 'https://ambiance-ensitech.me/api'
+  };
