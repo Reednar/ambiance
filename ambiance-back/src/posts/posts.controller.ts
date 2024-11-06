dc165237ad0a684002805fbb0f27bb83df11acbf
Reednar <lepieloic94@gmail.com>
2024-11-06 18:42:25 +0100

    feat(posts.controller.ts): add function fetch all posts

--- ambiance-back/src/posts/posts.controller.ts
+++ ambiance-back/src/posts/posts.controller.ts
@@ -7,6 +7,38 @@ import { PostsService } from './posts.service';
 export class PostsController {
   constructor(private postsService: PostsService) {}
 
+  @Get()
+  @ApiOperation({ summary: 'Return all posts' })
+  @ApiResponse({
+    status: 200,
+    description: 'Successful response',
+    examples: {
+      example1: {
+        summary: 'Successful response example',
+        value: [
+          {
+            idPublication: 1,
+            codePostal: '78000',
+            rue: '2',
+            ville: 'Montigny',
+            titre: 'Cinéma',
+            dateEvenement: '2024-11-06T10:36:19.000Z',
+            description: 'Scary movie',
+            prix: '12.00',
+            lien: 'cineugc.com',
+            dateCreation: '2024-11-06T10:36:58.000Z',
+            participantMax: 10,
+            participantMin: 2,
+            typePost: 'activité',
+          },
+        ],
+      },
+    },
+  })
+  async getPosts() {
+    return await this.postsService.findAll();
+  }
+
   @Get(':id')
   @ApiOperation({ summary: 'Return one post by id' })
   @ApiResponse({
