c406291b63e54194e9edcb56edb23b6385577916
Andrews-Hef <andrews.agricole@gmail.com>
2024-11-07 14:45:23 +0100

    EDIT :pencil2: change controller folder

---
+++ ambiance-back/src/controllers/users/users.controller.ts
@@ -0,0 +1,5 @@
+import { Controller, Get, Param, NotFoundException } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+
+@Controller('users')
+export class UsersController {}
