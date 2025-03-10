1084d30d2a24a9f799966f77e4aabd181b15c8a6
Reednar <lepieloic94@gmail.com>
2025-03-10 10:10:53 +0100

    feat(eslintrc): loosening rules linter

--- ambiance-back/.eslintrc.js
+++ ambiance-back/.eslintrc.js
@@ -15,18 +15,29 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js', 'dist/'], // Exclut le dossier dist
+  ignorePatterns: ['.eslintrc.js', 'dist/'], // Exclut le dossier de build
   rules: {
+    //Assouplissement des regles trop strictes
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': 'warn',
-    "prettier/prettier": [
-      "error",
+
+    // Pour les variables de type `any`
+    '@typescript-eslint/no-explicit-any': 'warn', // Autorisé mais signalé
+
+    // Gestion des variables non utilisées
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }], // Tolère les variables non utilisées commençant par `_`
+
+    // Gestion des promesses
+    '@typescript-eslint/no-floating-promises': 'error', // Interdit les promesses non gérées
+    '@typescript-eslint/require-await': 'error', // Interdit les `async` sans `await`
+
+    // Formatage du code
+    'prettier/prettier': [
+      'error',
       {
-        "endOfLine": "auto"
-      }
+        endOfLine: 'auto',
+      },
     ],
   },
 };
