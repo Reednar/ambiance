b52aa01fa28ee8a08aa3683699fdfaa5093316c4
Reednar <lepieloic94@gmail.com>
2025-03-12 10:56:35 +0100

    update url api

--- ambiance-front/src/app/service/test/test.service.ts
+++ ambiance-front/src/app/service/test/test.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class TestService {
-  private apiUrl = 'http://localhost:3000/api'; // URL du backend NestJS
+  private apiUrl = '/api'; // URL du backend NestJS
 
   constructor(private http: HttpClient) {}
 
