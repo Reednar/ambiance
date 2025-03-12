f1cb31b53023ed973eb0e94fb3caa99f0fec7ff8
Reednar <lepieloic94@gmail.com>
2025-03-12 13:09:37 +0100

    restore file

--- ambiance-front/src/app/app.component.ts
+++ ambiance-front/src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent implements OnInit {
     this.testService.getData().subscribe(
       (response) => {
         this.data = response;
-        console.log('Data received: ', this.data); // Test build jenkins webhook
+        console.log('Data received: ', this.data);
       },
       (error) => {
         console.error('Error fetching data: ', error);
