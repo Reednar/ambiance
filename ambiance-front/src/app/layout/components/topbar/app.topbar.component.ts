a95c67ddcd409054aa347d520909cd7729cb43a5
Reednar <lepieloic94@gmail.com>
2025-04-09 09:53:57 +0200

    fix(topbar): change menu item when user is connected

--- ambiance-front/src/app/layout/components/topbar/app.topbar.component.ts
+++ ambiance-front/src/app/layout/components/topbar/app.topbar.component.ts
@@ -3,7 +3,7 @@ import { MenuItem } from 'primeng/api';
 import { LayoutService } from "../../service/app.layout.service";
 import { AuthService } from '../../../service/authent.service'; // Assure-toi que AuthService est bien importé
 import { Router } from '@angular/router'; // Assure-toi que Router est bien importé
-import { OnInit, OnChanges } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-topbar',
@@ -11,72 +11,39 @@ import { OnInit, OnChanges } from '@angular/core';
   styleUrl: './app.topbar.component.scss'
 
 })
-export class AppTopBarComponent implements OnInit, OnChanges {
+export class AppTopBarComponent implements OnInit {
 
-  items!: MenuItem[];
+  items: MenuItem[] | undefined;
   menuItems: any[] = [];
   @ViewChild('menubutton') menuButton!: ElementRef;
   @ViewChild('topbarmenubutton') topbarMenuButton!: ElementRef;
   @ViewChild('topbarmenu') menu!: ElementRef;
-  currentUrl: string;
-  textUrl!: string;
   isConnected: any;
 
-  constructor(private authService: AuthService, private router: Router, public layoutService: LayoutService, private cdr: ChangeDetectorRef) {
-    this.currentUrl = window.location.href; // Récupère l'URL actuelle
-    if (this.currentUrl.includes("localhost:4200")) {
-      this.textUrl = "Local";
-    }
-    else if (this.currentUrl.includes("dev-alize-gmao-noprod")) {
-      this.textUrl = "DevNoProd";
-    }
-    else if (this.currentUrl.includes("dev-alize-gmao.azurewebsites")) {
-      this.textUrl = "Dev";
-    }
-    else {
-      this.textUrl = "Prod";
-    }
-  }
+  constructor(private authService: AuthService, private router: Router, public layoutService: LayoutService, private cdr: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     // Initialisation du menu selon l'état actuel
     this.isConnected = this.authService.isConnected$.subscribe((value) => {
-      this.isConnected = value
+      this.isConnected = value; // Met à jour la valeur de isConnected
+      this.updateMenuItems(); // Met à jour les éléments du menu en fonction de l'état de la connexion
     })
 
     this.cdr.markForCheck();  // Forcer Angular à vérifier les changements
-    this.updateMenuItems();
-  }
-
-  ngOnChanges(): void {
-    this.isConnected = this.authService.isConnected$.subscribe((value) => {
-      this.isConnected = value
-    })
-    this.updateMenuItems();
-  }
-
-  ngAfterViewInit(): void {
-    this.updateMenuItems();
-    this.cdr.detectChanges();  // Force la détection des changements après la vue
   }
 
   // Fonction pour mettre à jour les éléments du menu en fonction de l'état de la connexion
   updateMenuItems(): void {
-    console.log('Mise à jour des éléments du menu');
-    if (this.authService.isLoggedIn()) {
-      console.log('Utilisateur connecté');
-      this.menuItems = [
+    if (this.isConnected) {
+      this.items = [
         { label: 'Déconnexion', icon: 'pi pi-sign-out', command: () => this.logout() }
       ];
     } else {
-      console.log('Utilisateur non connecté');
-      this.menuItems = [
+      this.items = [
         { label: 'Connexion', icon: 'pi pi-sign-in', routerLink: '/login' },
         { label: 'Inscription', icon: 'pi pi-user-plus', routerLink: '/register' }
       ];
     }
-
-
   }
 
   // Fonction de déconnexion
@@ -85,14 +52,4 @@ export class AppTopBarComponent implements OnInit, OnChanges {
     this.updateMenuItems();  // Mettre à jour le menu après la déconnexion
     this.router.navigate(['/login']);  // Rediriger vers la page d'accueil après la déconnexion
   }
-
-  loggedInMenuItems = [
-    { label: 'Logout', icon: 'pi pi-sign-out', command: () => this.logout() }
-  ];
-
-  loggedOutMenuItems = [
-    { label: 'Login', icon: 'pi pi-sign-in', routerLink: '/login' },
-    { label: 'Register', icon: 'pi pi-user-plus', routerLink: '/register' }
-  ];
-
 }
