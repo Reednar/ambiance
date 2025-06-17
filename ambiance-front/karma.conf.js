44ba943374ad248907d8024ed7572c77e03ab8db
Reednar <lepieloic94@gmail.com>
2025-06-17 20:06:39 +0200

    test coverage

---
+++ ambiance-front/karma.conf.js
@@ -0,0 +1,25 @@
+module.exports = function (config) {
+  config.set({
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-coverage'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-spec-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+    ],
+    reporters: ['spec', 'coverage'],
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+        { type: 'lcov' }, // Important pour SonarQube
+      ],
+    },
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
+  });
+};
