4d718aed3e65b5447ca4f5df6fbd41a7d7f4e663
Reednar <lepieloic94@gmail.com>
2025-06-24 18:38:52 +0200

    test

--- ambiance-back/src/app.controller.spec.ts
+++ ambiance-back/src/app.controller.spec.ts
@@ -17,7 +17,7 @@ describe('AppController', () => {
   describe('root', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toStrictEqual({
-        message: 'Hello World!',
+        message: 'Hello World!!',
       });
     });
   });
