9511f4ce5a069463f061f837d367c1525891e2df
Reednar <lepieloic94@gmail.com>
2025-06-30 22:16:24 +0200

    update script test

--- ambiance-front/karma.conf.js
+++ ambiance-front/karma.conf.js
@@ -13,12 +13,17 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
+      captureConsole: true, // Capture les logs de console
       jasmine: {
-        // you can add configuration options for Jasmine here
+        random: false, // Désactive l'exécution aléatoire des tests
+        failFast: true, // Arrête au premier échec (optionnel)
+        timeoutInterval: 10000 // Augmente le timeout si nécessaire
       },
+      clearContext: false // Garde les résultats visibles
     },
     jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      suppressAll: true, // Supprime les traces dupliquées
+      suppressFailed: false // Montre les échecs
     },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/ambiance-front-'),
@@ -27,9 +32,21 @@ module.exports = function (config) {
         { type: 'html' },
         { type: 'text-summary' },
         { type: 'lcov' }
-      ]
+      ],
+      check: {
+        global: {
+          statements: 50,
+          branches: 40,
+          functions: 45,
+          lines: 50
+        }
+      }
     },
     reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
     browsers: ['ChromeHeadlessNoSandbox'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
@@ -37,10 +54,17 @@ module.exports = function (config) {
         flags: [
           '--no-sandbox',
           '--disable-gpu',
-          '--disable-dev-shm-usage'
+          '--disable-dev-shm-usage',
+          '--headless',
+          '--remote-debugging-port=9222'
         ]
       }
     },
-    restartOnFileChange: true
+    singleRun: true,
+    restartOnFileChange: false,
+    failOnEmptyTestSuite: false, // Ne pas échouer si aucun test trouvé
+    browserDisconnectTimeout: 10000,
+    browserDisconnectTolerance: 3,
+    browserNoActivityTimeout: 60000
   });
 };
